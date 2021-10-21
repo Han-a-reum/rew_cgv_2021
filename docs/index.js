86879994219a1953ff7d6bfafcf3bfcd69f24fae
Han-a-reum <hanar0323@naver.com>
2021-10-21 10:20:19 +0900

    22

--- docs/index.js
+++ docs/index.js
@@ -1,35 +1,18 @@
 function SwiperBox1__init() {
   const swiper = new Swiper(".swiper-box-1 .swiper", {
-    loop:true,
-    speed : 1000,
+    effect: "cards",
+    grabCursor: true,
+    loop: true,
     autoplay: {
-      delay: 2800,
+      delay: 2000,
+      disableOnInteraction: false,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      renderBullet: function (index, className) {
-        let txt = "";
-
-        if ( index == 0 ) {
-          txt = "[EVENT] 향기까지 취향대로";
-        }
-        else if ( index == 1 ) {
-          txt = "[EVENT] 버터와 오일이 만나면?";
-        }
-        else if ( index == 2 ) {
-          txt = "[EVENT] 할로윈 에디션 론칭!";
-        }
-        else if ( index == 3 ) {
-          txt = "[EVENT] 다운로드 러쉬 9월";
-        }
-        else if ( index == 4 ) {
-          txt = "[EVENT] 다음을 기대하세요!";
-        }
-
-        className += ' index-' + index;
-
-        return '<span class="' + className + '"><div>' + txt + "</div></span>";
+    creativeEffect: {
+      prev: {
+        translate: [0, 0, -400],
+      },
+      next: {
+        translate: ["100%", 0, 0],
       },
     },
   });
